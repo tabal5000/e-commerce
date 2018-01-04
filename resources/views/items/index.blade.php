e4e84c8bf47d1e8714d63e23c7966acd6e8df000
Tabs <iz6968@student.uni-lj.si>
2018-01-04 13:16:52 +0100

    Stuff

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -3,13 +3,132 @@
 @section('content')
 <div class="panel panel-default">
   <div class="panel panel-body">
-    <ul>
+    <!-- <ul>
         @foreach ($items as $item)
             <li>
               <a href="/items/{{$item->id}}"> {{ $item->title }} </a>
             </li>
         @endforeach
-    </ul>
+    </ul> -->
+    <div class="row">
+
+      <div class="col-md-3 col-sm-6">
+        <div class="thumbnail">
+          <img src="/storage/images/svetlo.png" class="img-responsive resize">
+            <div class="caption">
+              <h4> Application! </h4>
+            </div>
+        </div>
+      </div>
+
+      <div class="col-md-3 col-sm-6">
+        <div class="thumbnail">
+          <img src="/storage/images/svetlo.png" class="img-responsive resize">
+            <div class="caption">
+              <a href="#"> Application! </h4>
+            </div>
+        </div>
+      </div>
+
+      <div class="col-md-3 col-sm-6">
+        <div class="thumbnail">
+          <img src="/storage/images/svetlo.png" class="img-responsive resize">
+            <div class="caption">
+              <h4> Application! </h4>
+            </div>
+        </div>
+      </div>
+
+      <div class="col-md-3 col-sm-6">
+        <div class="thumbnail">
+          <img src="/storage/images/svetlo.png" class="img-responsive resize">
+            <div class="caption">
+              <h4> Application! </h4>
+            </div>
+        </div>
+      </div>
+
+    </div>
+
+    <div class="row">
+
+      <div class="col-md-3 col-sm-6">
+        <div class="thumbnail">
+          <img src="/storage/images/svetlo.png" class="img-responsive resize">
+            <div class="caption">
+              <h4> Application! </h4>
+            </div>
+        </div>
+      </div>
+
+      <div class="col-md-3 col-sm-6">
+        <div class="thumbnail">
+          <img src="/storage/images/svetlo.png" class="img-responsive resize">
+            <div class="caption">
+              <h4> Application! </h4>
+            </div>
+        </div>
+      </div>
+
+      <div class="col-md-3 col-sm-6">
+        <div class="thumbnail">
+          <img src="/storage/images/svetlo.png" class="img-responsive resize">
+            <div class="caption">
+              <h4> Application! </h4>
+            </div>
+        </div>
+      </div>
+
+      <div class="col-md-3 col-sm-6">
+        <div class="thumbnail">
+          <img src="/storage/images/svetlo.png" class="img-responsive resize">
+            <div class="caption">
+              <h4> Application! </h4>
+            </div>
+        </div>
+      </div>
+
+    </div>
+
+    <div class="row">
+
+      <div class="col-md-3 col-sm-6">
+        <div class="thumbnail">
+          <img src="/storage/images/svetlo.png" class="img-responsive resize">
+            <div class="caption">
+              <h4> Application! </h4>
+            </div>
+        </div>
+      </div>
+
+      <div class="col-md-3 col-sm-6">
+        <div class="thumbnail">
+          <img src="/storage/images/svetlo.png" class="img-responsive resize">
+            <div class="caption">
+              <h4> Application! </h4>
+            </div>
+        </div>
+      </div>
+
+      <div class="col-md-3 col-sm-6">
+        <div class="thumbnail">
+          <img src="/storage/images/svetlo.png" class="img-responsive resize">
+            <div class="caption">
+              <h4> Application! </h4>
+            </div>
+        </div>
+      </div>
+
+      <div class="col-md-3 col-sm-6">
+        <div class="thumbnail">
+          <img src="/storage/images/svetlo.png" class="img-responsive resize">
+            <div class="caption">
+              <h4> Application! </h4>
+            </div>
+        </div>
+      </div>
+
+    </div>
   </div>
 </div>
 @endsection
--- resources/views/items/show1.blade.php
+++ resources/views/items/show1.blade.php
@@ -11,19 +11,23 @@
 
                   <div class="panel-body" id="itemBody">
 
-                    <div class="col-md-4 row-eq-height">
+                    <div class="col-md-4">
                         <img class="img-responsive center-block" src="/storage/images/{{$item->img}}" alt="">
                     </div>
 
-                    <div class="col-md-8 row-eq-height">
+                    <div class="col-md-8">
                             <div class="caption-full row">
                                 <h1 class="pull-left"> {{$item->title}}
-                                <h1 class="pull-right">{{$item->price}}€</h1>
                             </div>
                             <div class="row">
                               <p id="itemDescription"> {{$item->description}} </p>
                             </div>
-                            <button class="btn btn-success" id="addToCartBtn"> Wii </button>
+                            <div class="row">
+                              <h1>{{$item->price}}€</h1>
+                            </div>
+                            <div class="row">
+                              <button class="btn btn-success" id="addToCartBtn"> Add to cart </button>
+                            </div>
                     </div>
 
                   </div>
